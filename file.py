c3e7574351d8bbf63c8fc78bc2e53305c2221c1a
riles-hub <arileshaddouche@gmail.com>
2021-03-25 21:17:57 +0100

    function main

---
+++ file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env pyhton3
+
+from book import Book
+
+def main():
+    book = Book("TEST")
+    book.insert(10, 10.0, 0)
+    book.insert(120, 12.0, 1)
+    book.insert_order(5, 10.0, 0)
+    book.insert_order(2, 11.0, 0)
+    book.insert_order(1, 11.0, 1)
+    book.insert_order(10, 10.0, 1)
+
+    if __name__ == "__main__":
+        main()
+    
+
